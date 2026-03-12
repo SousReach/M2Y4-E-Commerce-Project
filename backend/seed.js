aafa56d1caab6228f52faff361b5ef37b39581c0
Suos Reach <soousreach@gmail.com>
2026-03-12 13:33:29 +0700

    Product

--- backend/seed.js
+++ backend/seed.js
@@ -35,7 +35,7 @@ const seedDB = async () => {
       {
         name: 'Blazer',
         description: 'Wear this to the party like you deserve it daddy.',
-        price: 1.00,
+        price: 999.99,
         images: ['https://static01.nyt.com/images/2025/05/05/multimedia/05MET-GALA-LIVEBLOG-DIDDY-01-pqvt/05MET-GALA-LIVEBLOG-DIDDY-01-pqvt-articleLarge-v2.jpg?quality=75&auto=webp&disable=upscale'],
         category: catMap['Dresses'],
         sizes: ['S', 'M', 'L', 'XL'],
@@ -43,11 +43,11 @@ const seedDB = async () => {
         stock: 50,
         isFeatured: true,
       },
-      
+
       {
         name: 'Quarter Zip',
         description: 'If you want to end up in the files, wear this bad boy for your island vacation.',
-        price: 1.00,
+        price: 150.00,
         images: ['https://static01.nyt.com/images/2026/02/04/fashion/04epstein-clothes/04epstein-clothes-articleLarge.jpg?quality=75&auto=webp&disable=upscale'],
         category: catMap['T-Shirts'],
         sizes: ['28', '30', '32', '34', '36'],
@@ -59,7 +59,7 @@ const seedDB = async () => {
       {
         name: 'KC-Hat',
         description: 'Recommended by the best barber in the game. The best hast for the best stroker in the gang.',
-        price: 1.00,
+        price: 100.00,
         images: ['https://pbs.twimg.com/media/FhIgQlsWYAA9bID.jpg'],
         category: catMap['Accessories'],
         sizes: ['28', '30', '32', '34', '36'],
@@ -71,7 +71,7 @@ const seedDB = async () => {
       {
         name: 'Orange Hoodie',
         description: 'Wear this if messi is the goat',
-        price: 1.00,
+        price: 67.00,
         images: ['https://campaignme.com/wp-content/uploads/2022/03/talabat-_-Cristiano-Ronaldo-2.jpg'],
         category: catMap['T-Shirts'],
         sizes: ['28', '30', '32', '34', '36'],
@@ -83,11 +83,23 @@ const seedDB = async () => {
       {
         name: 'Dior Sweater',
         description: 'Feeling like oppa and taking penalty over dog ass club while ghosting in big games.',
-        price: 1.00,
+        price: 1500.00,
         images: ['https://versus.uk.com/wp-content/uploads/2025/08/64759600f36e14ee9b88a992_267549651_304332918257098_9186883737337548800_n-800x1000-1.jpeg'],
         category: catMap['T-Shirts'],
         sizes: ['28', '30', '32', '34', '36'],
         colors: ['Sand', 'Black', 'White'],
+        stock: 30,
+        isFeatured: true,
+      },
+
+      {
+        name: 'Audemars Piguet Royal Oak Perpetual Calendar 41 Skeleton Ceramic',
+        description: 'Flex on the huzz with this watch.',
+        price: 528000.00,
+        images: ['https://watchlab.ae/upload/iblock/9c3/3meosh19f0ht8xvd2jw2rflm9pt3mvle.jpg'],
+        category: catMap['Accessories'],
+        sizes: ['28', '30', '32', '34', '36'],
+        colors: ['Black'],
         stock: 30,
         isFeatured: true,
       },
